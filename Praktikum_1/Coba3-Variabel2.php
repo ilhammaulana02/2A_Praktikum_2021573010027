acf938929c2b7670eeb082ce83e685eb8395440c
ilhammaulana02 <ilhammaulana020603@gmail.com>
2022-09-21 16:59:13 +0700

    praktikum pertama

--- Praktikum_1/Coba3-Variabel2.php
+++ Praktikum_1/Coba3-Variabel2.php
@@ -1,7 +1,7 @@
 <html>
     <body>
         <?php
-            $jalan = "Medan - B. Aceh";
+            $jalan = "Medan - B.Aceh ";
             $noRumah = 29;
             $blok = "4C";
             $jumlahPenghuni = 3;
@@ -14,6 +14,6 @@
             echo "Hasil Variabel variabel blok + variabel noRumah adalah ";
             echo $hasil = $blok + $noRumah;
             echo "<br>";
-            ?>
+        ?>
     </body>
 </html>
