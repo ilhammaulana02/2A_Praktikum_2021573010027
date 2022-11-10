9569b0713028f264ec92eac88a07a57ebb6f1019
ilhammaulana02 <ilhammaulana020603@gmail.com>
2022-11-10 09:05:55 +0700

    melanjutkan projek decafe

--- DeCafe/header.php
+++ DeCafe/header.php
@@ -22,7 +22,7 @@
                                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                                         </svg>Profile</a></li>
-                                <li><a class="dropdown-item" href="#">
+                                <li><a class="dropdown-item" href="logout">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power me-2 mb-1" viewBox="0 0 16 16">
                                             <path d="M7.5 1v7h1V1h-1z" />
                                             <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z" />
